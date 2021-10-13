cf5e99044cd9df6878dc2e0836a38b5cd7132eda
Marco Romano <35372961+MarcooRo@users.noreply.github.com>
2021-10-13 23:35:11 +0200

    add filter.js

---
+++ filter.js
@@ -0,0 +1,28 @@
+const RMRK = "RMRK::LIST::1.0.0::8949162-e0b9bdcc456a36497a-KANBIRD-KANS-00000001";
+
+const List = "LIST";
+const Buy = "BUY";
+const Sell = "SELL";
+const Mint = "MINT";
+const version_1 = "1.0.0";
+const version_2 = "2.0.0";
+const XXX = "e0b9bdcc456a36497a";
+const Kanbird = "KANBIRD";
+const KanbirdSuperFounder = "KANS";
+const KanbirdFounder = "KANF";
+const KanbirdRare = "KANR";
+const KanbirdLimited = "KANL";
+const KanBack = "KANBACK";
+const KanBg = "KANBG";
+const KanFrnt = "KANFRNT";
+const KanHead = "KANHEAD";
+const KanHand = "KANHAND";
+const KanChest = "KANCHEST";
+const Img = ""; // come trovare il Json?
+const linkCatalogo = "https://kanaria.rmrk.app/catalogue/";
+const linkIpfs = "https://rmrk.mypinata.cloud/ipfs/";
+
+
+if (RMRK.includes(version_1)) {
+    console.log('version_1');
+}
