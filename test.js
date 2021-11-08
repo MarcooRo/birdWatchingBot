7cab1c0dd775e7ca8c0de4e1d22ba5ed11b5f800
Alberto <tabez@libero.it>
2021-11-08 15:22:22 +0100

    ok

--- test.js
+++
@@ -1,7 +0,0 @@
-const getMessageGivenFilter = require('./catchEvent/scriptUtils/filter.js')
-const sendMessage = require('./catchEvent/scriptUtils/sendMessage.js')
-var remark = "RMRK::LIST::2.0.0::8788594-e0b9bdcc456a36497a-KANCHEST-2b55_necklace-00002207::2185000000000";
-
-// console.log(getMessageGivenFilter.buildMessage(remark))
-// sendMessage.sendMessage("1238654632", getMessageGivenFilter.buildMessage(remark))
-sendMessage.sendPhoto("1238654632", getMessageGivenFilter.buildMessage(remark).imgSRC, getMessageGivenFilter.buildMessage(remark).print())
