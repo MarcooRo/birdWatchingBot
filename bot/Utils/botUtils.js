5b8512354d26edda74ed00230ab6c81f64d115f7
Marco Romano <35372961+MarcooRo@users.noreply.github.com>
2021-11-10 23:16:58 +0100

    trad

--- bot/Utils/botUtils.js
+++ bot/Utils/botUtils.js
@@ -5,41 +5,41 @@ const rocket = '\u{1F680}'
 const back = '\u{1F519}'
 const dollar = '\u{1F4B5}'
 
-exports.sendStartMenu = function sendStartMenu (ctx, inExecution, bot) {
+exports.sendStartMenu = function sendStartMenu(ctx, inExecution, bot) {
     const startMessage = "Welcome on Bird Watching Bot";
     chatId = ctx.chat.id
-    if(!inExecution){
-    bot.telegram.sendMessage(ctx.chat.id, startMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: `Create Filter ${start}`, callback_data: 'filtra'}
+    if (!inExecution) {
+        bot.telegram.sendMessage(ctx.chat.id, startMessage, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: `Create Filter ${start}`, callback_data: 'filtra' }
+                    ]
                 ]
-            ]
-        }
-    })}
-    else {
+            }
+        })
+    } else {
         bot.telegram.sendMessage(ctx.chat.id, startMessage, {
             reply_markup: {
 
                 inline_keyboard: [
                     [
-                        {text: `Create Filter ${start}`, callback_data: 'filtra'}
+                        { text: `Create Filter ${start}`, callback_data: 'filtra' }
                     ],
                     [
-                        {text: `Stop ${stop}`, callback_data: 'Stop'}
+                        { text: `Stop ${stop}`, callback_data: 'Stop' }
                     ]
                 ]
             }
         })
-    } 
+    }
 }
 
 
 exports.doStop = function doStop(ctx, db, bot) {
     //reset filter 
     ctx.deleteMessage()
-    if(db.deleteUser(ctx.chat.id) == -1){
+    if (db.deleteUser(ctx.chat.id) == -1) {
         bot.telegram.sendMessage(ctx.chat.id, "YOU HAVE NEVER STARTED THE BOT", {
             reply_markup: {
                 remove_keyboard: true
@@ -51,50 +51,50 @@ exports.doStop = function doStop(ctx, db, bot) {
             remove_keyboard: true
         }
     })
-    this.sendStartMenu(ctx,0,bot);
+    this.sendStartMenu(ctx, 0, bot);
 }
 let ref;
-exports.sendFilterMenu = function sendFilterMenu(ctx, bot, filter){
+exports.sendFilterMenu = function sendFilterMenu(ctx, bot, filter) {
     const menuMessage = "Create your filter!"
     ctx.deleteMessage();
     bot.telegram.sendMessage(ctx.chat.id, menuMessage, {
         reply_markup: {
             inline_keyboard: [
                 [
-                    {text: (filter.allList ? `All ${checked}`: 'All') , callback_data: 'allList'},
-                    {text: (filter.allBird ? `all Birds ${checked}`: 'all Birds') , callback_data: 'allBird'}
+                    { text: (filter.allList ? `All ${checked}` : 'All'), callback_data: 'allList' },
+                    { text: (filter.allBird ? `all Birds ${checked}` : 'all Birds'), callback_data: 'allBird' }
                 ],
                 [
-                    {text: (filter.SuperFunder ? `Super Funder ${checked}`:'Super Funder'), callback_data: 'SuperFunder'},
-                    {text: (filter.Funder ? `Funder ${checked}`:'Funder'), callback_data: 'Funder'}
+                    { text: (filter.SuperFunder ? `Super Funder ${checked}` : 'Super Funder'), callback_data: 'SuperFunder' },
+                    { text: (filter.Funder ? `Funder ${checked}` : 'Funder'), callback_data: 'Funder' }
                 ],
                 [
-                    {text: (filter.Rare ? `Rare ${checked}`:'Rare'), callback_data: 'Rare'},
-                    {text: (filter.Limited ? `Limited ${checked}`:'Limited'), callback_data: 'Limited'}
+                    { text: (filter.Rare ? `Rare ${checked}` : 'Rare'), callback_data: 'Rare' },
+                    { text: (filter.Limited ? `Limited ${checked}` : 'Limited'), callback_data: 'Limited' }
                 ],
                 [
-                    {text: (filter.BackPack ? `BackPack ${checked}`:'BackPack'), callback_data: 'BackPack'},
-                    {text: (filter.Background ? `Background ${checked}`:'Background'), callback_data: 'Background'}
+                    { text: (filter.BackPack ? `BackPack ${checked}` : 'BackPack'), callback_data: 'BackPack' },
+                    { text: (filter.Background ? `Background ${checked}` : 'Background'), callback_data: 'Background' }
                 ],
                 [
-                    {text: (filter.ForeGround ? `ForeGround ${checked}`:'ForeGround'), callback_data: 'ForeGround'},
-                    {text: (filter.Headwear ? `Headwear ${checked}`:'Headwear'), callback_data: 'Headwear'}
+                    { text: (filter.ForeGround ? `ForeGround ${checked}` : 'ForeGround'), callback_data: 'ForeGround' },
+                    { text: (filter.Headwear ? `Headwear ${checked}` : 'Headwear'), callback_data: 'Headwear' }
                 ],
                 [
-                    {text: (filter.Handheld ? `Handheld ${checked}`:'Handheld'), callback_data: 'Handheld'},
-                    {text: (filter.Necklace ? `Necklace ${checked}`:'Necklace'), callback_data: 'Necklace'}
+                    { text: (filter.Handheld ? `Handheld ${checked}` : 'Handheld'), callback_data: 'Handheld' },
+                    { text: (filter.Necklace ? `Necklace ${checked}` : 'Necklace'), callback_data: 'Necklace' }
                 ],
                 [
-                    {text: `Set Price Limit ${dollar}`, callback_data: 'price'}
+                    { text: `Set Price Limit ${dollar}`, callback_data: 'price' }
                 ],
                 [
-                    {text: `Salva e avvia ${rocket}`, callback_data: 'Start'}
+                    { text: `Save and Start ${rocket}`, callback_data: 'Start' }
                 ],
                 [
-                    {text: `Indietro ${back}`, callback_data: 'Indietro'}
+                    { text: `Back ${back}`, callback_data: 'Indietro' }
                 ]
             ]
         }
     })
-    
+
 }
