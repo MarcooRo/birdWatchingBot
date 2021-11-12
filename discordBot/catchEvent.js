ca6b4ed134f130990c614e9d11e6a5cfbfd1eda7
Alberto <tabez@libero.it>
2021-11-12 12:22:20 +0100

    discord work

--- discordBot/catchEvent.js
+++ discordBot/catchEvent.js
@@ -1,56 +1,34 @@
 const ApiPromise = require('@polkadot/api')
 const WsProvider = require('@polkadot/api')
 const hexToString = require('@polkadot/util')
-const filterUtils = require('./scriptUtils/filter.js')
-const sendMessage = require('./scriptUtils/sendMessage.js');
-const messageCreator = require('./scriptUtils/messageCreator.js');
-const prepareImg = require('./scriptUtils/prepareImage.js')
-//const discord = require('../discordBot/discordBot.js')
-let pool = require('./scriptUtils/Db.js')
+const messageCreator = require('./botUtils/messageCreator.js');
 
 
-const getRemark = async function getRemark(api, hederNumber) {
+const getRemark = async function getRemark(api, hederNumber, channel) {
     const blockHash = await api.rpc.chain.getBlockHash(hederNumber);
     const signedBlock = await api.rpc.chain.getBlock(blockHash);
     signedBlock.block.extrinsics.forEach((ex, index) => {
         if (ex.method.meta.name.toString() == "remark") {
             var remarks = hexToString.hexToString(ex.args.toString());
-            if (remarks.includes("2.0.0") && (remarks.includes("LIST") || remarks.includes("BUY"))) { // get only LIST & 2.0.0
-                //send discord notificationx
+            if (remarks.includes("2.0.0")) { // get only LIST & 2.0.0
                 let message = messageCreator.buildMessage(remarks)
-                if(message.price != 0) {
-                    //discord.sendRmrkDiscord(message.print(), null, "LIST")
-                    let messageFilter = filterUtils.prepareFilterMesage(remarks)
-                    //if(remarks.includes("KANBIRD"))prepareImg.prepareImg(message)
-                    pool.pool.getConnection(function(err, connection) {
-                        pool.pool.query(`Select * from Users`, (err, result, fields) =>{
-                            if(err) return console.log(err)
-                            console.log(remarks)
-                            for(let k in result) {
-                                if(filterUtils.checkFilterMessage_User(messageFilter, result[k].filter, result[k].priceLimit))
-                                    sendMessage.sendPhoto(result[k].chatId.toString(), message, message.print())
-                            }
-                        })
-                        connection.release()
-                    })
-                }
+                console.log(remarks)
+                    if(remarks.includes("LIST")) channel.List.send({ embeds: [message] })
+                    if(remarks.includes("BUY")) channel.Buy.send({ embeds: [message] })
              }
         }
     });
     return "";
 }
 
-function botStart() {
+exports.botStart = function botStart(channel) {
     (async() => {
         console.log("SCAN STARTED")
         const provider = new WsProvider.WsProvider('wss://kusama-rpc.polkadot.io/');
         const api = await ApiPromise.ApiPromise.create({ provider });
-        //const VERSION = "2.0.0";
             const blockNumber = await api.rpc.chain.subscribeNewHeads((header) => {
                 console.log("Blocco numero " + header.number)
-                getRemark(api, header.number)
+                getRemark(api, header.number, channel)
             });
     })()
 }
-
-botStart()
--- discordBot/discordBot.js
+++ discordBot/discordBot.js
@@ -1,19 +1,49 @@
-const {discordBot, Intents, Client, Discord, MessageAttachment} = require('discord.js')
+const {discordBot, Intents, Client, Discord, MessageEmbed} = require('discord.js')
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const messageCreator = require('./botUtils/messageCreator.js');
 const token = process.env.BOT_DISCORD_TOKEN
+const botStart = require('./catchEvent.js')
 bot.login("OTA3NDE0MDc2MjgyNTIzNzAw.YYm1Ng.RAkoZko32XCOAKwi9P2hEH7Bfqs")
 const channelList = bot.channels.cache.find(channel => channel.id === '907415732432814101')
 const channelBuy = bot.channels.cache.find(channel => channel.id === '907415768893915237')
+
+function test(channelList, text){
+    channelList.send(text)
+}
 
 
 bot.on("message", function(message) {
     const channelList = bot.channels.cache.find(channel => channel.id === '907415732432814101')
     const channelBuy = bot.channels.cache.find(channel => channel.id === '907415768893915237')
+    let channels = {
+        List: channelList,
+        Buy: channelBuy
+    }
     switch(message.content){
         case '!START':
+            botStart.botStart(channels)
+        case '!TEST':
+            channels.List.send({embeds:[(messageCreator.buildMessage("RMRK::LIST::2.0.0::8949171-e0b9bdcc456a36497a-KANBIRD-KANL-00007714::3788600000000"))]})
+
             //sendRmrkDiscord("","","")
-			const attach = new MessageAttachment('https://i.imgur.com/DabI2Cq.png')
-			message.channel.send(`https://i.imgur.com/DabI2Cq.png ciao ciao`, attach)
+			//const embed = new MessageEmbed() //Ver 11.5.1 of Discord.js
+            // .setColor('#0099ff')
+            // .setTitle('Some title')
+            // .setURL('https://discord.js.org/')
+            // .setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+            // .setDescription('Some description here')
+            // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+            // .addFields(
+            //     { name: 'Regular field title', value: 'Some value here' },
+            //     { name: '\u200B', value: '\u200B' },
+            //     { name: 'Inline field title', value: 'Some value here', inline: true },
+            //     { name: 'Inline field title', value: 'Some value here', inline: true },
+            // )
+            // .addField('Inline field title', 'Some value here', true)
+            // .setImage('https://i.imgur.com/AfFp7pu.png')
+            // .setTimestamp()
+            // .setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+            // channelList.send({ embeds: [embed] });
 
     }
 })
