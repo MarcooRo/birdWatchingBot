78723772ffa396c8a91570d480f3b896fc746b7a
Alberto <tabez@libero.it>
2021-11-16 11:23:59 +0100

    working version

--- catchEvent/test1_0_0.js
+++
@@ -1,7 +0,0 @@
-const test = require('./1-0-0.js')
-const message = require('../catchEvent/scriptUtils/1-0-0/sendMessage.js')
-const build = require('../catchEvent/scriptUtils/1-0-0/messageCreator.js')
-
-//test.manage1_0_0('RMRK::LIST::1.0.0::10081238-7e982817a2d2575031-MAGK1-01_BABY_KUSAMA-0000000000000001::245000000000')
-let mess = build.buildMessage('RMRK::LIST::1.0.0::10081238-7e982817a2d2575031-MAGK1-01_BABY_KUSAMA-0000000000000001::245000000000')
-message.sendPhoto("1238654632", mess,mess.print() )
