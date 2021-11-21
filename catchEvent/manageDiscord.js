1d345153b52feabb12fd28d38df21f719d3dae41
Alberto <tabez@libero.it>
2021-11-21 17:45:23 +0100

    dup version

--- catchEvent/manageDiscord.js
+++ catchEvent/manageDiscord.js
@@ -2,7 +2,15 @@ const discordMessage = require('./scriptUtils/discordUtils/messageCreator.js')
 
 exports.manageDiscord = function manageDiscord(remark, channel) {
     if(channel.isAlive() == 1) {
-        if(remark.includes("LIST"))channel.getChannels().List.send(discordMessage.buildMessage(remark).print)
-        if(remark.includes("BUY"))channel.getChannels().Buy.send(discordMessage.buildMessage(remark).print)
+        
+        if(remark.includes("2.0.0")) {
+            if(remark.includes("LIST"))channel.getChannels().List2.send(({embeds:[discordMessage.buildMessage(remark)]}))
+            if(remark.includes("BUY"))channel.getChannels().Buy2.send({embeds:[discordMessage.buildMessage(remark)]})
+        }
+
+        if(remark.includes("1.0.0")) {
+            if(remark.includes("LIST"))channel.getChannels().List1.send(({embeds:[discordMessage.buildMessage(remark)]}))
+            if(remark.includes("BUY"))channel.getChannels().Buy1.send({embeds:[discordMessage.buildMessage(remark)]})
+        }
     }
 }
