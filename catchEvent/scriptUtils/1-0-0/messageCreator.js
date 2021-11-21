7b4dd62f0b35317ec59e21de75b6297cfccd9c00
Marco Romano <marcoromanoweb@gmail.com>
2021-11-21 14:05:49 +0100

    fix price

--- catchEvent/scriptUtils/1-0-0/messageCreator.js
+++ catchEvent/scriptUtils/1-0-0/messageCreator.js
@@ -6,13 +6,13 @@ const linkCatalogo = "https://singular.rmrk.app/collectibles/";
 const linkIpfs = "https://rmrk.mypinata.cloud/ipfs/";
 
 exports.buildMessage = function buildMessage(remark) {
-    var remarkSplit = remark.split('::'); // rmrk::LIST::1.0.0::5105000-0aff6865bed3a66b-VALHELLO-POTION_HEAL-0000000000000001::10000000000
+    var remarkSplit = remark.split('::'); // rmrk::LIST::1.0.0::5105000-0aff6865bed3a66b-VALHELLO-POTION_HEAL-0000000000000001::1000000000000
     var remarkNft = remarkSplit[3]; // 5105000-0aff6865bed3a66b-VALHELLO-POTION_HEAL-0000000000000001
     var nftType = remarkNft.split('-');
     
     var nftName =  nftType[3] + ' (n° '+ nftType[4] +')'; // POTION_HEAL (n° 0000000000000001)
     var collection = nftType[2] + ' ('+ nftType[1] +')'; // VALHELLO (0aff6865bed3a66b)
-    var remarkPrice = (remarkSplit[4] / 1000000000000) / 0.95
+    var remarkPrice = (remarkSplit[4] / 1000000000000) / 0.98 
      remarkPrice = Number(remarkPrice)
     remarkPrice = remarkPrice.toFixed(2)
     remarkPrice = Number(remarkPrice)
