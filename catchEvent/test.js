8b4c3635f7d1d4696a6bbe7fe752bb3726b1069d
Alberto <tabez@libero.it>
2021-11-23 23:11:41 +0100

    fix

--- catchEvent/test.js
+++ catchEvent/test.js
@@ -1,9 +1,9 @@
-const messageCreator = require('./scriptUtils/1-0-0/messageCreator.js');
-const sendMessage = require('./scriptUtils/1-0-0/sendMessage.js');
-const filterUtils = require('./scriptUtils/1-0-0/filter.js')
+const messageCreator = require('./scriptUtils/2-0-0/messageCreator.js');
+const sendMessage = require('./scriptUtils/2-0-0/sendMessage.js');
+const filterUtils = require('./scriptUtils/2-0-0/filter.js')
 const discordTest = require ('./manageDiscord.js')
-
-let message = messageCreator.buildMessage('RMRK::LIST::2.0.0::8788590-e0b9bdcc456a36497a-KANCHEST-269b-fe0f_necklace-00001722::1567500000000')
-let filterMessage = filterUtils.prepareFilterMesage('RMRK::LIST::2.0.0::8788590-e0b9bdcc456a36497a-DONKEY-269b-fe0f_necklace-00001722::1567500000000')
-console.log(filterUtils.checkFilterMessage_User(filterMessage,'0', "donkey", "0", '10'))
-//sendMessage.sendPhoto("1238654632",message, message.print())
+let rmrk = 'RMRK::LIST::2.0.0::8949162-e0b9bdcc456a36497a-KANBIRD-KANR-00000353::142500000000000';
+let message = messageCreator.buildMessage(rmrk)
+let filterMessage = filterUtils.prepareFilterMesage(rmrk)
+console.log(filterUtils.checkFilterMessage_User(filterMessage,'000000000000', '0'))
+sendMessage.sendPhoto("1238654632",message, message.print())
