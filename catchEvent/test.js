e742504d2d33874c62aa6be017817aab1507afb9
Alberto <tabez@libero.it>
2021-11-24 21:50:36 +0100

    fix and clean

--- catchEvent/test.js
+++
@@ -1,9 +0,0 @@
-const messageCreator = require('./scriptUtils/2-0-0/messageCreator.js');
-const sendMessage = require('./scriptUtils/2-0-0/sendMessage.js');
-const filterUtils = require('./scriptUtils/2-0-0/filter.js')
-const discordTest = require ('./manageDiscord.js')
-let rmrk = 'RMRK::LIST::2.0.0::8949162-e0b9bdcc456a36497a-KANBIRD-KANR-00000353::142500000000000';
-let message = messageCreator.buildMessage(rmrk)
-let filterMessage = filterUtils.prepareFilterMesage(rmrk)
-console.log(filterUtils.checkFilterMessage_User(filterMessage,'000000000000', '0'))
-sendMessage.sendPhoto("1238654632",message, message.print())
--- catchEvent/test2.js
+++
@@ -1,3 +0,0 @@
-const messageC = require('./scriptUtils/discordUtils/messageCreator1-0-0.js');
-let mex = messageC.buildMessage('rmrk::LIST::1.0.0::5105000-0aff6865bed3a66b-VALHELLO-POTION_HEAL-0000000000000001::1000000000000');
-console.log(mex)
