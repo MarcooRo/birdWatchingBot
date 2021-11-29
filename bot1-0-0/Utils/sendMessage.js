db45228dbccae6e023486474eab4e66274dddebc
Marco Romano <marcoromanoweb@gmail.com>
2021-11-29 23:19:39 +0100

    Update sendMessage.js

--- bot1-0-0/Utils/sendMessage.js
+++ bot1-0-0/Utils/sendMessage.js
@@ -1,5 +1,5 @@
 // BOT Connection
-const TOKEN = "1991386673:AAHd1SiiQAzRVDpM0liFyJYmWs4G3Bg5YMM";
+const TOKEN = "xxxxxx:xxxxxxxxxxxxxxxxx";//1991386673:AAHd1SiiQAzRVDpM0liFyJYmWs4G3Bg5YMM
 const axios = require('axios')
 
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
